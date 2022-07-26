593a77bfa319f6426fece720af1ae6506cc4b4f2
Salih Alperen <sal.alperen1@gmail.com>
2022-07-26 23:39:42 +0200

    Added page routes and some packages

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import About from './About';
+import Dashboard from './Dashboard';
+import Landing from './Landing';
+import SharedLayout from './SharedLayout';
+import Error from './Error';
+
+export { About, Dashboard, Landing, SharedLayout, Error };
